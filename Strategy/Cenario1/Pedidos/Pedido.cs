874352eb104755f7aec8a3709f77195869a4f3e1
Larksel <lemuelgames@outlook.com>
2025-03-17 22:43:15 -0300

    :sparkles: feat: Strategy - Cenário 1

---
+++ Strategy/Cenario1/Pedidos/Pedido.cs
@@ -0,0 +1,17 @@
+﻿namespace Cenario1.Pedidos
+{
+    public class Pedido
+    {
+        public double Valor { get; set; }
+
+        public double CalculaFreteComum()
+        {
+            return Valor * 0.05;
+        }
+
+        public double CalculaFreteExpresso()
+        {
+            return Valor * 0.1;
+        }
+    }
+}
---
+++ Strategy/Cenario1/Program.cs
@@ -0,0 +1,12 @@
+﻿using Cenario1.Pedidos;
+
+// Criação do pedido
+Pedido pedido = new Pedido();
+
+// Atribuição do valor
+pedido.Valor = 100;
+
+// Cálculo para frete comum
+Console.WriteLine("Frete Comum: R$" + pedido.CalculaFreteComum());
+// Cálculo para frete expresso
+Console.WriteLine("Frete Expresso: R$" + pedido.CalculaFreteExpresso());
