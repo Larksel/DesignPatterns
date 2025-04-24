5a3e5a8181e1058d95b932ec58eec6650726f03e
Larksel <lemuelgames@outlook.com>
2025-04-23 21:59:09 -0300

    :sparkles: feat: Facade

---
+++ FacadeProblema/FacadeProblema/facade/FacadeVendas.cs
@@ -0,0 +1,50 @@
+﻿using FacadeProblema.cadastros;
+using FacadeProblema.vendas;
+
+namespace FacadeProblema.facade;
+
+public class FacadeVendas
+{
+    private Pedido pedido;
+    private Pagamento pagamento;
+    private EmailPedido email;
+
+    public FacadeVendas(Consumidor consumidor)
+    {
+        pedido = new Pedido();
+        pedido.consumidor = consumidor;
+
+        email = new EmailPedido(pedido);
+    }
+
+    public void addProduto(Produto produto)
+    {
+        pedido.addProduto(produto);
+    }
+
+    public void pedidoCredito()
+    {
+        pagamento = new PagamentoCredito(pedido);
+        if (pagamento.realizarPagamento())
+        {
+            email.enviarEmail("Pagamento realizado com sucesso (crédito)");
+        } 
+        else
+        {
+            email.enviarEmail("Falha no pagamento do pedido");
+        }
+    }
+
+    public void pedidoBoleto()
+    {
+        pagamento = new PagamentoBoleto(pedido);
+        if (pagamento.realizarPagamento())
+        {
+            email.enviarEmail("Pagamento realizado com sucesso (boleto)");
+        } 
+        else
+        {
+            email.enviarEmail("Falha no pagamento do pedido");
+        }
+    }
+}
